e29a2b8302db6500864f2598b9d637696c132ab6
veruoz <0688691@gmail.com>
2022-06-06 11:56:05 +0300

    Pizzas add to Cart

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,13 +1,14 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types'
 import classNames from "classnames";
+import Button from "../Button";
 
-const PizzaBlock = ({ name, imageUrl, price, types, sizes }) => {
+const PizzaBlock = ({ id, name, imageUrl, price, types, sizes, onClickAddPizza, addedCount }) => {
     const availableTypes = ['тонкое', 'традиционное']
     const availableSize = [26, 30, 40]
     // чтобы активным было поле, где есть тот или другой индекс берем первый элемент массива
     const [activeType, setActiveType] = useState(types[0]);
-    const [activeSize, setActiveSize] = useState(sizes[0]);
+    const [activeSize, setActiveSize] = useState(0);
 
     const onSelectType = (index) => {
         setActiveType(index)
@@ -17,6 +18,17 @@ const PizzaBlock = ({ name, imageUrl, price, types, sizes }) => {
     }
     // console.log(name, types)
 
+    const onAddPizza = () => {
+        const obj = {
+            id,
+            name,
+            imageUrl,
+            price,
+            size: availableSize[activeSize],
+            type: availableTypes[activeType]
+        }
+        onClickAddPizza(obj)
+    }
 
 
     return (
@@ -59,7 +71,7 @@ const PizzaBlock = ({ name, imageUrl, price, types, sizes }) => {
             </div>
             <div className="pizza-block__bottom">
                 <div className="pizza-block__price">от {price} ₽</div>
-                <div className="button button--outline button--add">
+                <Button onClick={onAddPizza} className="button--add" outline>
                     <svg
                         width="12"
                         height="12"
@@ -73,26 +85,28 @@ const PizzaBlock = ({ name, imageUrl, price, types, sizes }) => {
                         />
                     </svg>
                     <span>Добавить</span>
-                    <i>2</i>
-                </div>
+                    {addedCount && <i>{addedCount}</i>}
+                </Button>
             </div>
         </div>
     );
 };
 
 PizzaBlock.propTypes = {
-    name: PropTypes.string,
-    imageUrl: PropTypes.string,
-    price: PropTypes.number,
-    types: PropTypes.arrayOf(PropTypes.number),
-    sizes: PropTypes.arrayOf(PropTypes.number),
+    name:            PropTypes.string,
+    imageUrl:        PropTypes.string,
+    price:           PropTypes.number,
+    types:           PropTypes.arrayOf(PropTypes.number),
+    sizes:           PropTypes.arrayOf(PropTypes.number),
+    onClickAddPizza: PropTypes.func,
+    addedCount: PropTypes.number,
 }
 
 PizzaBlock.defaultProps = {
-    name: '---',
+    name:  '---',
     price: 0,
     types: [],
-    sizes: []
+    sizes: [],
 }
 
 export default PizzaBlock;
--- src/components/SortPopup.jsx
+++ src/components/SortPopup.jsx
@@ -12,8 +12,10 @@ const SortPopup = React.memo(function SortPopup ({ items, activeSortType, onClic
         setVisiblePopup(!visiblePopup)
     }
 
-    const handleOutsideClick = (e) => {
-        if (!e.path.includes(sortRef.current)) {
+    const handleOutsideClick = (event) => {
+        // * решение проблемы с path firefox
+        const path = event.path || (event.composedPath && event.composedPath())
+        if (!path.includes(sortRef.current)) {
             setVisiblePopup(false)
         }
     }
